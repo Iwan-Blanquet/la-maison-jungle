f50645e70165e266aeab1ec26f6ba64934c190c9
Iwan-Blanquet <blanquetiwan@gmail.com>
2022-03-10 16:48:48 +0100

    Ajout de data et intégration

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 /* Importation des fichiers nécessaire de notre composant */
 import Banner from './Banner'
 import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 function App() {
   return (
   <div>
     <Banner />
     <Cart />
+    <ShoppingList />
   </div>
   )
 }

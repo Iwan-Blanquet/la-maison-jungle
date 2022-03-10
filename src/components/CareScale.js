fb0c4b018d710a89db9e45fe2c27bec65eba50c1
Iwan-Blanquet <blanquetiwan@gmail.com>
2022-03-10 21:21:40 +0100

    Ajout de PlantItem

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,6 +1,13 @@
+import Sun from '../assets/Sun.svg'
+import Water from '../assets/Water.svg'
+
 function CareScale({scaleValue, careType}) {
     const range = [1, 2, 3]
-    const scaleType = careType === 'light' ? '☀️' : '💧'
+    const scaleType = careType === 'light' ? (
+        <img src={Sun} alt='sun-icon' />
+    ) : (
+        <img src={Water} alt='water-icon' />
+    )
     return (
         <div>
             {range.map((rangeElem) => scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null)}
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,7 @@
 // Import des ficgiers nécessaire au composant ShoppingList
 import '../styles/ShoppingList.css'
 import { plantList } from "../datas/plantList"
-import CareScale from './CareScale'
+import PlantItem from './PlantItem'
 
 function ShoppingList() {
     const categories = plantList.reduce(
@@ -17,13 +17,14 @@ function ShoppingList() {
                 ))}
             </ul>
             <ul className='lmj-plant-list'>
-                {plantList.map((plant) => (
-                    <li key={plant.id} className='lmj-plant-item'>
-                        {plant.name}
-                        {plant.isBestSale && <span>🔥</span>}
-                        <CareScale careType='water' scaleValue={plant.water} />
-                        <CareScale careType='light' scaleValue={plant.light} />
-                    </li>
+                {plantList.map(({id, cover, name, light, water}) => (
+                    <PlantItem 
+                        id={id}
+                        cover={cover}
+                        name={name}
+                        light={light}
+                        water={water}
+                    />
                 ))}
             </ul>
         </div>
